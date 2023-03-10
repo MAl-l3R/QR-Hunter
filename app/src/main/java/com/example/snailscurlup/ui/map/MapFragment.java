ee052291193ef95e8b8b04cab47880fcf42abb85
Zgell <zgellner2001@gmail.com>
2023-03-09 17:22:53 -0700

    Got map integration working!

--- app/src/main/java/com/example/snailscurlup/ui/map/MapFragment.java
+++ app/src/main/java/com/example/snailscurlup/ui/map/MapFragment.java
@@ -1,21 +1,43 @@
 package com.example.snailscurlup.ui.map;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+// import com.example.snailscurlup.databinding.ActivityMapsBinding;
 import com.example.snailscurlup.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +47,13 @@ public class MapFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private GoogleMap qrmap;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    // private ActivityMapsBinding binding;
+    double userLongitude = 0;  // Set initial values so that onMapReady() doesn't crash
+    double userLatitude = 0;
 
     public MapFragment() {
         // Required empty public constructor
@@ -48,6 +77,39 @@ public class MapFragment extends Fragment {
         return fragment;
     }
 
+    public void fetchCurrentUserLocation() {
+        // Get user location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+
+        if (ContextCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
+
+            fusedLocationProviderClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null)
+                    .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if (location != null) {
+
+                                try {
+                                    Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    Double latitude = addressList.get(0).getLatitude();
+                                    Double longitude = addressList.get(0).getLongitude();
+                                    String address = addressList.get(0).getAddressLine(0);
+
+                                    // TODO: UPLOAD LOCATION TO FIREBASE
+
+                                } catch (IOException e) {
+                                    System.out.println("Exception occurred with location");
+                                }
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(getContext(), "You need to enable Location permission from Settings", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,12 +117,26 @@ public class MapFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        this.fetchCurrentUserLocation();
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        qrmap = googleMap;
+        // Get user position to center the map there
+        LatLng userPosition = new LatLng(this.userLongitude, this.userLatitude);
+        qrmap.moveCamera(CameraUpdateFactory.newLatLng(userPosition));
     }
 }
