1a976c8ecf8ae9fb0168c2b0fd54eb1bd7330b31
Zgell <zgellner2001@gmail.com>
2023-03-13 10:22:33 -0600

    Applied stashed changes from different version of branch

---
+++ app/src/androidTest/java/com/example/snailscurlup/CreateAccountTest.java
@@ -0,0 +1,81 @@
+package com.example.snailscurlup;
+
+import android.app.Activity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import android.widget.EditText;
+import android.widget.ListView;
+import com.robotium.solo.Solo;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Random;
+
+public class CreateAccountTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    /**
+     * Checks if the application can create an account from the initial screen.
+     */
+    @Test
+    public void checkCreateAccount(){
+        /**
+         * Tests to see if the user can create an account.
+         * Synthesizes random user credentials and creates a new account.
+         */
+        // Assertion for current activity being the main one
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        // Go to Create Account page
+        solo.clickOnView(solo.getView(R.id.create_account_button));
+
+        // Fill in account details
+        // Generate a random number to attach to account so it can be tested
+        // many times without wiping app data
+        final int randomLowerBound = 10000000;
+        final int randomUpperBound = 99999999;
+        Random random = new Random();
+        int randomUsernameID = random.nextInt(randomUpperBound - randomLowerBound) + randomLowerBound;
+
+        // Assemble some fake info to provide to account registration
+        String dummyUsername = "testuser" + Integer.toString(randomUsernameID);
+        String dummyEmail = dummyUsername + "@email.com";
+        String dummyPhone = "1234567890";
+
+        // Enter in fake information for account registration
+        solo.enterText((EditText) solo.getView(R.id.username_field), dummyUsername);
+        solo.enterText((EditText) solo.getView(R.id.Email_field), dummyEmail);
+        solo.enterText((EditText) solo.getView(R.id.phonenumber_field), dummyPhone);
+
+        // Proceed with account creation
+        solo.clickOnView(solo.getView(R.id.create_account_button));
+
+        // Test to see if username is recognized
+        String welcomeMsg = dummyUsername;
+        assertTrue(solo.waitForText(welcomeMsg, 1, 3000));
+    }
+}
