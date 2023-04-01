a964c3a7ac367d3542a1d7a39b486cc39b013489
Ayan Babwany <babwany@ualberta.ca>
2023-04-01 07:21:51 -0600

    modified QrGalleryAdapter, and added call with fragment manager so Dialog fragment for abstract Qr shows when click on it

--- app/src/main/java/com/example/snailscurlup/ui/scan/QrGalleryAdapter.java
+++ app/src/main/java/com/example/snailscurlup/ui/scan/QrGalleryAdapter.java
@@ -1,41 +1,39 @@
 package com.example.snailscurlup.ui.scan;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
-import static java.text.DateFormat.getDateTimeInstance;
-
 import android.content.Context;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.snailscurlup.R;
 import com.squareup.picasso.Picasso;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
-
-
-public class QrGalleryAdapter extends RecyclerView.Adapter<QrGalleryAdapter.Viewholder>{
+public class QrGalleryAdapter extends RecyclerView.Adapter<QrGalleryAdapter.Viewholder> {
 
     private final Context context;
-   /*** commented out for new Abstract
-    * private final ArrayList<QrCode> QRCodeArrayList; **/
+    /*** commented out for new Abstract
+     * private final ArrayList<QrCode> QRCodeArrayList; **/
 
-   private final ArrayList<QRCodeInstanceNew> QRCodeArrayList;
+    private final ArrayList<QRCodeInstanceNew> QRCodeArrayList;
+    private final FragmentManager fragmentManager; // add FragmentManager variable
 
 
     // Constructor
-    public QrGalleryAdapter(Context context, ArrayList<QRCodeInstanceNew> QRCodeArrayList) {
+    public QrGalleryAdapter(Context context, ArrayList<QRCodeInstanceNew> QRCodeArrayList, FragmentManager fragmentManager) {
         this.context = context;
-        this.QRCodeArrayList= QRCodeArrayList;
+        this.QRCodeArrayList = QRCodeArrayList;
+        this.fragmentManager = fragmentManager;
     }
 
     @NonNull
@@ -56,31 +54,31 @@ public class QrGalleryAdapter extends RecyclerView.Adapter<QrGalleryAdapter.View
         // format timestamp with simple Date format
         String pattern = "E, dd MMM yyyy HH:mm:ss z";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        String formattedTimestamp =  simpleDateFormat .format(singleqrcode.getScanQRLogTimeStamp());
+        String formattedTimestamp = simpleDateFormat.format(singleqrcode.getScanQRLogTimeStamp());
         holder.QRCodeTimeStamp.setText(formattedTimestamp);
         Picasso.get().load(singleqrcode.getURL()).into(holder.QrCodeVisual);
 
 
-        //to send item click on to view page
+        // set click listener on the item view
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // get the position of the clicked item
+                int position = holder.getAdapterPosition();
+                // get the QR code instance corresponding to the clicked item
+                QRCodeInstanceNew clickedQRCode = QRCodeArrayList.get(position);
 
-                QRCodeInstanceNew clickedQrCodeItem = QRCodeArrayList.get(holder.getAdapterPosition());
-
-                Intent intent = new Intent(context, QRCodeInfoFragment.class);
-                intent.putExtra("hash", clickedQrCodeItem.getAbstractQR().getHash());
-                intent.putExtra("url", clickedQrCodeItem.getAbstractQR().getURL());
-                intent.putExtra("name", clickedQrCodeItem.getAbstractQR().getName());
-                intent.putExtra("points", clickedQrCodeItem.getAbstractQR().getPointsInt());
-                context.startActivity(intent);
+                QRInfoDialogFragment dialogFragment = QRInfoDialogFragment.newInstance("Dialog Title", "Dialog Message");
+                Bundle bundle = new Bundle();
+                bundle.putString("clickedQRCodeHash", clickedQRCode.AbstractQRHash());
+                dialogFragment.setArguments(bundle);
+                dialogFragment.show(fragmentManager, "CustomDialogFragment");
 
 
+                // create an intent to start the QR code details activity
 
             }
         });
-
-
 
     }
 
@@ -95,19 +93,19 @@ public class QrGalleryAdapter extends RecyclerView.Adapter<QrGalleryAdapter.View
     }
 
     // View holder class for initializing of your views such as TextView and Imageview
-    public static class Viewholder extends RecyclerView.ViewHolder{
+    public static class Viewholder extends RecyclerView.ViewHolder {
         private final ImageView QrCodeVisual;
         private final TextView QRCodeName;
         private final TextView QRCodeScore;
 
         private final TextView QRCodeTimeStamp;
 
-        public Viewholder(@NonNull View itemView){
+        public Viewholder(@NonNull View itemView) {
             super(itemView);
-            QrCodeVisual= itemView.findViewById(R.id.qrcode_visual);
-            QRCodeName= itemView.findViewById(R.id.qrcode_namefield);
-            QRCodeScore= itemView.findViewById(R.id.qrcode_scorefield);
-            QRCodeTimeStamp= itemView.findViewById(R.id.qrcode_timestampfield);
+            QrCodeVisual = itemView.findViewById(R.id.qrcode_visual);
+            QRCodeName = itemView.findViewById(R.id.qrcode_namefield);
+            QRCodeScore = itemView.findViewById(R.id.qrcode_scorefield);
+            QRCodeTimeStamp = itemView.findViewById(R.id.qrcode_timestampfield);
         }
     }
 }
