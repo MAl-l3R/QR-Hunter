9789e33a4982d1f3f48dcc14cb8ebc58d37e64c0
Richard He <richardhe330@gmail.com>
2023-04-01 19:28:13 -0600

    renamed class and refined comments

--- app/src/main/java/com/example/snailscurlup/ui/leaderboard/Leader.java
+++ app/src/main/java/com/example/snailscurlup/ui/leaderboard/Leader.java
@@ -2,50 +2,33 @@ package com.example.snailscurlup.ui.leaderboard;
 
 /**
  * Leader class represents a user on the leaderboard. This class contains
- * information about the user such as their username, email, and phone number.
- *
- * @author Richard He
- * @version 1.0
- * @since 2023-04-01
+ * information about the user such as their username and score
  */
 public class Leader {
     private String username;
-    private String email;
-    private String phone;
+    private String score;
+
 
     /**
-     * Constructs a new Leader with the specified username, email, and phone number.
-     *
+     * Constructs a new Leader with the specified username and score.
      * @param username The user's username.
-     * @param email The user's email.
-     * @param phone The user's phone number.
+     * @param score The user's score.
      */
-    Leader(String username, String email, String phone){
+    Leader(String username, String score){
         this.username=username;
-        this.email=email;
-        this.phone=phone;
+        this.score = score;
     }
 
     /**
-     * Returns the user's username.
-     *
      * @return The user's username.
      */
     String getUsername(){return this.username;}
+
     /**
-     * Returns the user's phone number.
-     *
      * @return The user's phone number.
      */
-    String getEmail(){
-        return this.email;
+    String getScore(){
+        return this.score;
     }
-    /**
-     * Returns the user's phone number.
-     *
-     * @return The user's phone number.
-     */
-    String getPhone(){
-        return this.phone;
-    }
+
 }
